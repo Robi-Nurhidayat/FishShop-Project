ed2d28bfbfb47e70f62ab13c3f9b28d5a9431de9
Bagus Perdana <bagus.10118249@mahasiswa.unikom.ac.id>
2022-03-15 22:59:04 +0700

    Import Font dan Bootstrap

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 // load template for ejs
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'))
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
 // routing
 
-app.get('/',function(req,res){
-    res.render('home')
-})
-
-
+app.get("/", function (req, res) {
+    res.render("home");
+});
+app.get("/produk", function (req, res) {
+    res.render("produk");
+});
 
 // end of routing
 
-// typer localhost:3000 for access 
-app.listen(3000,function(){
-    console.log('Server running on port 3000');
-})
+// typer localhost:3000 for access
+app.listen(3000, function () {
+    console.log("Server running on port 3000");
+});
